b93dcd6f84336e72956e34a262dff3c264a62fb6
Ap97 <anthony66661@live.com>
2024-07-03 01:25:09 -0500

    avance primera parte de informacion de curso

--- parte1/courseinfo/src/App.jsx
+++ parte1/courseinfo/src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const  Header = (nombCurso) => {
+  return(
+    <div>
+      <h1>Nombre del curso: {nombCurso.nombreCurso}</h1>
+    </div>
+  );
+}
+
+const Parte = (infoP) => {
+  return(
+    <>
+      <p>{infoP.parte} {infoP.ejercicio}</p>
+    </>
+  )
+}
+
+const Content = (infoS) => {
+  return(
+    <div>
+      <Parte parte = {infoS.parte1} ejercicio={infoS.ejercicio1}/>
+      <Parte parte = {infoS.parte2} ejercicio={infoS.ejercicio2}/>
+      <Parte parte = {infoS.parte3} ejercicio={infoS.ejercicio3}/>
+    </div>
+  );
+}
+
+const Total = (elementos) => {
+  return(
+    <>
+    <p>Numero de ejercicios: {elementos.num1 + elementos.num2 + elementos.num3}</p>
+    </>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const curso = 'Desarrollo de aplicaciones Half Stack';
+  const parte1 = 'Fundamentos de React';
+  const ejercicio1 = 10;
+  const parte2 = 'Usando props para el paso de datos';
+  const ejercicio2 = 7;
+  const parte3 = 'Estado de un componente';
+  const ejercicio3 = 14;
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header nombreCurso = {curso}/>
+      <Content parte1 = {parte1} ejercicio1 = {ejercicio1} parte2 = {parte2} ejercicio2 = {ejercicio2} parte3 = {parte3} ejercicio3 = {ejercicio3}/>
+      <Total num1 = {ejercicio1} num2 = {ejercicio2} num3 = {ejercicio3}/>
+    </div>
   )
 }
 
